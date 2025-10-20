d94f3200e535974c5e7e26319c06ae05bef80f31
crossflame22 <owenator07@gmail.com>
2025-10-20 11:37:39 -0400

    added inline markdown splits

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-    test = TextNode("This is a test string", TextType.PLAIN_TEXT)
+    test = TextNode("This is a test string", TextType.TEXT)
     print(test)
 
 main()
