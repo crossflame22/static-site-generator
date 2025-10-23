dfc4316f18544eafecb166aff213591454fa6fa6
crossflame22 <owenator07@gmail.com>
2025-10-23 13:23:52 -0400

    test?

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
+
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
@@ -12,8 +13,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
         else:
             generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
+
 def generate_page(from_path, template_path, dest_path, basepath):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
     from_file.close()
@@ -24,12 +26,12 @@ def generate_page(from_path, template_path, dest_path, basepath):
 
     node = markdown_to_html_node(markdown_content)
     html = node.to_html()
-    
+
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
-    template = template.replace('href="/', f'href={basepath}')
-    template = template.replace('src="/', f'src="{basepath}')
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -43,3 +45,4 @@ def extract_title(md):
     for line in lines:
         if line.startswith("# "):
             return line[2:]
+    raise ValueError("no title found")
